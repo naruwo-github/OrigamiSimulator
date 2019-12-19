b12d1e33d9d03164575b90307a8e1b66e8280663
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-12-19 17:39:58 +0900

    格子を描画可能に

--- jsExtended/drawApp.js
+++ jsExtended/drawApp.js
@@ -188,14 +188,12 @@ function initDrawApp(globals) {
     //格子を描画する(デフォルトはマゼンタ？)
     if (gridTool.points.length > 0) {
       context.fillStyle = lineColors[0];
-      for (var i = 0; i < gridTool.points.length; i+=2) {
+      for (let i = 0; i < gridTool.points.length; i++) {
         const stl1 = gridTool.points[i];
-        const stl2 = gridTool.points[i+1];
         context.fillRect(stl1[0]-3, stl1[1]-3, 7, 7);
-        context.fillRect(stl2[0]-3, stl2[1]-3, 7, 7);
       }
       if(gridTool.points.length%4 == 0) {
-        drawGrid(context, gridTool.points);
+        drawGrid(context, lineColors[3], gridTool.points);
       }
     }
   }
@@ -615,15 +613,35 @@ function initDrawApp(globals) {
   }
 
   //格子を描画する
-  function drawGrid(ctx, array) {
-    for (let index = 0; index < array.length; index+=2) {
-      const element1 = array[index];
-      const element2 = array[index+1];
-      const p0 = element1[0];
-      const p1 = element1[1];
-      const p2 = element2[0];
-      const p3 = element2[1];
-      drawLine(ctx, lineColors[3], 2, element1[0], element1[1], element2[0], element2[1]);
+  function drawGrid(ctx, color, array) {
+    const lines = 10;
+    for (let index = 0; index < array.length; index+=4) {
+      const element0 = array[index];
+      const element1 = array[index+1];
+      const element2 = array[index+2];
+      const element3 = array[index+3];
+      const vectorP0 = new THREE.Vector2(element0[0], element0[1]);
+      const vectorP1 = new THREE.Vector2(element1[0], element1[1]);
+      const vectorP2 = new THREE.Vector2(element2[0], element2[1]);
+      const vectorP3 = new THREE.Vector2(element3[0], element3[1]);
+      const vectorP0P1 = new THREE.Vector2(vectorP1.x-vectorP0.x, vectorP1.y-vectorP0.y);
+      const vectorP1P2 = new THREE.Vector2(vectorP2.x-vectorP1.x, vectorP2.y-vectorP1.y);
+      const vectorP2P3 = new THREE.Vector2(vectorP3.x-vectorP2.x, vectorP3.y-vectorP2.y);
+      const vectorP3P0 = new THREE.Vector2(vectorP0.x-vectorP3.x, vectorP0.y-vectorP3.y);
+      vectorP0P1.divideScalar(lines);
+      vectorP1P2.divideScalar(lines);
+      vectorP2P3.divideScalar(lines);
+      vectorP3P0.divideScalar(lines);
+
+      for (let index = 1; index < lines; index++) {
+        var start = [vectorP0.x+vectorP0P1.x*index, vectorP0.y+vectorP0P1.y*index];
+        var end = [vectorP2.x+vectorP2P3.x*(10-index), vectorP2.y+vectorP2P3.y*(10-index)];
+        drawLine(ctx, color, 2, start[0], start[1], end[0], end[1]);
+
+        start = [vectorP1.x+vectorP1P2.x*index, vectorP1.y+vectorP1P2.y*index];
+        end = [vectorP3.x+vectorP3P0.x*(10-index), vectorP3.y+vectorP3P0.y*(10-index)];
+        drawLine(ctx, color, 2, start[0], start[1], end[0], end[1]);
+      }
     }
   }
 
